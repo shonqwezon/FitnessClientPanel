81dc1d2249db05b50c2017516c02c75f8197f870
Shon Qwezon <shonzbso@gmail.com>
2024-11-29 21:21:38 +0300

    feat: create table plan

---
+++ app/db/scripts/tables/plan.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS plan (
+    id SERIAL PRIMARY KEY,
+    base_cost NUMERIC(8, 2) NOT NULL,
+    begin_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    create_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    sportcenter_id INTEGER NOT NULL,
+    CONSTRAINT fk_sportcenter_id FOREIGN KEY (sportcenter_id) REFERENCES sportcenter(id)
+        ON UPDATE CASCADE ON DELETE SET NULL
+        DEFERRABLE INITIALLY DEFERRED -- Для создания таблиц без конфликтов
+);
