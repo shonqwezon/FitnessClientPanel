6f6eadfda0bf850a3573c51dcf2d774b768aadd3
Shon Qwezon <shonzbso@gmail.com>
2024-11-29 21:03:31 +0300

    fix: change REAL to NUMERIC(8, 2)

--- app/db/scripts/tables/service.sql
+++ app/db/scripts/tables/service.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS service (
     id SERIAL PRIMARY KEY,
     description TEXT NOT NULL,
-    cost REAL NOT NULL
+    cost NUMERIC(8, 2) NOT NULL
 );
 
 CREATE INDEX idx_description ON service (description);
