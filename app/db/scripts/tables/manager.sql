8c6295348c7c4a436250707f733487e0e2eb770d
Shon Qwezon <shonzbso@gmail.com>
2024-11-29 21:05:07 +0300

    fix: make sportcenter_id not null

--- app/db/scripts/tables/manager.sql
+++ app/db/scripts/tables/manager.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS manager (
     assigned_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     email VARCHAR(50) UNIQUE NOT NULL,
     password_hash VARCHAR(64) NOT NULL,
-    sportcenter_id INTEGER NULL,
+    sportcenter_id INTEGER NOT NULL,
     FOREIGN KEY (sportcenter_id) REFERENCES sportcenter(id)
         ON UPDATE CASCADE ON DELETE CASCADE
         DEFERRABLE INITIALLY DEFERRED -- Для создания таблиц без конфликтов
