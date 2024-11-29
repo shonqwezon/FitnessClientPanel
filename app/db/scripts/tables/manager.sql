cf8f1b88a5878b8a85bbfd66d1412eb7732593a9
Shon Qwezon <shonzbso@gmail.com>
2024-11-29 20:48:50 +0300

    feat: create manager table

---
+++ app/db/scripts/tables/manager.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS manager (
+    id SERIAL PRIMARY KEY,
+    fullname VARCHAR(50) NOT NULL,
+    assigned_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(64) NOT NULL,
+    sportcenter_id INTEGER NULL,
+    FOREIGN KEY (sportcenter_id) REFERENCES sportcenter(id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+        DEFERRABLE INITIALLY DEFERRED; -- Для создания таблиц без конфликтов
+);
+
+CREATE INDEX idx_fullname ON manager (fullname); -- Для быстрого поиска по имени
