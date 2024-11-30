ba909eaeb986efbd7b6f08c9b11cfe8dcd022ef5
Shon Qwezon <shonzbso@gmail.com>
2024-11-30 12:18:09 +0300

    feat: add creating tables

--- app/db/Database.py
+++ app/db/Database.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import psycopg2
 from psycopg2 import sql
 
@@ -11,8 +13,22 @@ logger = setup_logger(__name__)
 class Database:
     def __init__(self):
         logger.info("Init db")
-        self._pool = ConnectionManager()
+        self.pool = ConnectionManager()
+        self.create_tables()
+
+    def create_tables(self):
+        tables = [file for file in Path("app/db/scripts/tables").iterdir() if file.is_file()]
+        with self.pool.get_connection() as conn:
+            with conn.cursor() as cursor:
+                for table in tables:
+                    logger.debug(f"Creating {table}...")
+                    with open(table, "r", encoding="utf-8") as sql_file:
+                        cursor.execute(sql_file.read())
+                logger.debug("Adding constraints to tables...")
+                with open("app/db/scripts/constraints.sql", "r", encoding="utf-8") as sql_file:
+                    cursor.execute(sql_file.read())
+            conn.commit()
 
     def close(self):
         logger.info("Close db")
-        self._pool.free()
+        self.pool.free()
