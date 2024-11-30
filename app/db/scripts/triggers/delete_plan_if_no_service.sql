1d68e5cbdf835a53355bb52d5bbbccd7344439a6
Shon Qwezon <shonzbso@gmail.com>
2024-11-30 14:29:11 +0300

    fix: change func body

--- app/db/scripts/triggers/delete_plan_if_no_service.sql
+++ app/db/scripts/triggers/delete_plan_if_no_service.sql
@@ -1,26 +1,12 @@
-CREATE OR REPLACE FUNCTION set_plan_final_cost()
+CREATE OR REPLACE FUNCTION delete_plan_if_no_service()
 RETURNS TRIGGER AS
 $$
-DECLARE
-    plan_record RECORD;
-    total_service_cost NUMERIC(5, 2);
 BEGIN
-    SELECT p.base_cost, p.begin_time, p.end_time, sc.cost_ratio
-    INTO plan_record
-    FROM plan p
-    JOIN sportcenter sc ON sportcenter.id == p.sportcenter_id
-    WHERE p.id == NEW.plan_id;
+    IF NOT EXISTS (SELECT 1 FROM plan_service WHERE plan_id = OLD.plan_id) THEN
+        DELETE FROM plan WHERE id = OLD.plan_id;
+    END IF;
 
-    SELECT COALESCE(SUM(s.cost), 0)
-    INTO total_service_cost
-    FROM plan_service ps
-    JOIN service s ON ps.service_id = s.id
-    WHERE ps.plan_id = NEW.plan_id;
-
-    NEW.final_cost := (plan_record.base_cost + total_service_cost *
-        (EXTRACT(EPOCH FROM (plan_record.end_time - plan_record.begin_time)) / 3600)) * plan_record.cost_ratio;
-
-    RETURN NEW;
+    RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
 
