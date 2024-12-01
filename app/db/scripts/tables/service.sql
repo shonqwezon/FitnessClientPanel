fb6505652b230b345742efadb41db8bf89dbfe86
Shon Qwezon <shonzbso@gmail.com>
2024-12-01 23:07:53 +0300

    fix: make desc unique

--- app/db/scripts/tables/service.sql
+++ app/db/scripts/tables/service.sql
@@ -1,8 +1,6 @@
 CREATE TABLE IF NOT EXISTS service (
     id SERIAL PRIMARY KEY,
-    description TEXT NOT NULL,
+    description TEXT UNIQUE NOT NULL,
     cost NUMERIC(5, 2) NOT NULL,
     CONSTRAINT cost_check CHECK (cost >= 0)
 );
-
-CREATE INDEX IF NOT EXISTS idx_service_description ON service (description);
