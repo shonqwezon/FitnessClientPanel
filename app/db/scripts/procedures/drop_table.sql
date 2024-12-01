3d3f55a04eac3b18b638e40e4c36599bdaa9cdcb
Shon Qwezon <shonzbso@gmail.com>
2024-12-01 23:36:31 +0300

    fix: dropping table

--- app/db/scripts/procedures/drop_table.sql
+++ app/db/scripts/procedures/drop_table.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE PROCEDURE drop_table(table_name TEXT)
 AS $$
 BEGIN
+    EXECUTE FORMAT('DELETE FROM public.%I', table_name);
     EXECUTE FORMAT('DROP TABLE IF EXISTS public.%I CASCADE', table_name);
     RAISE NOTICE 'Table % has been dropped successfully.', table_name;
 END;
