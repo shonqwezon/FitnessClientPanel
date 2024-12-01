87305f19511f1de2c3fe56f0c1f068773f4ac90b
Shon Qwezon <shonzbso@gmail.com>
2024-12-01 11:56:03 +0300

    fix: make fullname unique

--- app/db/scripts/tables/manager.sql
+++ app/db/scripts/tables/manager.sql
@@ -1,10 +1,8 @@
 CREATE TABLE IF NOT EXISTS manager (
     id SERIAL PRIMARY KEY,
-    fullname VARCHAR(50) NOT NULL,
+    fullname VARCHAR(50) UNIQUE NOT NULL,
     assign_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     email VARCHAR(50) UNIQUE NOT NULL,
     password_hash VARCHAR(64) NOT NULL,
     sportcenter_id INTEGER NOT NULL
 );
-
-CREATE INDEX IF NOT EXISTS idx_manager_fullname ON manager (fullname); -- Для быстрого поиска по имени
