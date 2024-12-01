414de5c9c043797df282bcf20072d6aca8a451a8
Shon Qwezon <shonzbso@gmail.com>
2024-12-01 23:23:21 +0300

    feat: add_plan

---
+++ app/db/scripts/procedures/plan/add_plan.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE PROCEDURE add_plan(in_base_cost NUMERIC(8, 2),
+                                        in_begin_time TIME,
+                                        in_end_time TIME,
+                                        in_sportcenter_id INTEGER)
+AS $$
+DECLARE
+    new_id INTEGER;
+BEGIN
+    INSERT INTO plan (base_cost, begin_time, end_time, sportcenter_id)
+        VALUES (in_base_cost, in_begin_time, in_end_time, in_sportcenter_id) RETURNING id INTO new_id;
+    RAISE NOTICE 'Plan "%" has been added successfully.', new_id;
+END;
+$$ LANGUAGE plpgsql;
---
+++ app/db/scripts/triggers/check_plan_times.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION check_plan_time()
+RETURNS TRIGGER AS
+$$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM sportcenter s
+        WHERE s.id = NEW.sportcenter_id
+          AND (NEW.begin_time <= s.open_time OR NEW.end_time >= s.close_time)
+    ) THEN
+        RAISE EXCEPTION 'worker hours of plan must be beetwen worker hours of sportcenter';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER trigger_check_plan_time
+BEFORE INSERT ON plan
+FOR EACH ROW EXECUTE FUNCTION check_plan_time();
