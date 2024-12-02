b6aea10a9afbc63af5efe9e2ceb4dab2ed771c9d
Shon Qwezon <shonzbso@gmail.com>
2024-12-02 17:45:35 +0300

    feat: add funcs

---
+++ app/db/scripts/procedures/plan/get_plans_tech.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION app.get_plans_tech(in_sportcenter_id INTEGER)
+RETURNS TABLE (id INTEGER,
+                base_cost NUMERIC(8, 2),
+                begin_time TIME,
+                end_time TIME,
+                create_date TIMESTAMP)
+SECURITY DEFINER
+AS $$
+BEGIN
+    RETURN QUERY SELECT p.id,
+                        p.base_cost,
+                        p.begin_time,
+                        p.end_time,
+                        p.create_date
+                FROM plan AS p WHERE p.sportcenter_id = in_sportcenter_id;
+END;
+$$ LANGUAGE plpgsql;
--- app/db/scripts/triggers/set_client_plan_final_cost.sql
+++ app/db/scripts/triggers/set_client_plan_final_cost.sql
@@ -2,26 +2,9 @@ CREATE OR REPLACE FUNCTION set_plan_final_cost()
 RETURNS TRIGGER
 SECURITY DEFINER
 AS $$
-DECLARE
-    plan_record RECORD;
-    total_service_cost NUMERIC(5, 2);
 BEGIN
-    SELECT p.base_cost, p.begin_time, p.end_time, sc.cost_ratio
-    INTO plan_record
-    FROM plan p
-    JOIN sportcenter sc ON sportcenter.id == p.sportcenter_id
-    WHERE p.id == NEW.plan_id;
-
-    SELECT COALESCE(SUM(s.cost), 0)
-    INTO total_service_cost
-    FROM plan_service ps
-    JOIN service s ON ps.service_id = s.id
-    WHERE ps.plan_id = NEW.plan_id;
-
-    NEW.final_cost := (plan_record.base_cost + total_service_cost *
-        (EXTRACT(EPOCH FROM (plan_record.end_time - plan_record.begin_time)) / 3600)) * plan_record.cost_ratio;
-
-    RAISE NOTICE 'Final cost has been calculated.';
+    NEW.final_cost := app.calculate_plan_cost(NEW.plan_id);
+    RAISE NOTICE 'Final cost has been updated.';
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
