f2ef9df9bddbd339eaae3fb7f2170e9e188763f6
Shon Qwezon <shonzbso@gmail.com>
2024-12-02 20:36:30 +0300

    fix: make client unique

--- app/db/scripts/tables/client.sql
+++ app/db/scripts/tables/client.sql
@@ -1,9 +1,7 @@
 CREATE TABLE IF NOT EXISTS client (
     id SERIAL PRIMARY KEY,
-    fullname VARCHAR(50) NOT NULL,
+    fullname VARCHAR(50) UNIQUE NOT NULL,
     balance NUMERIC(8, 2) NOT NULL DEFAULT 0,
     reg_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT balance_check CHECK (balance >= 0)
 );
-
-CREATE INDEX IF NOT EXISTS idx_client_fullname ON client (fullname);
