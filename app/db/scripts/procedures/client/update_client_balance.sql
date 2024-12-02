532f07ac76ad123181e1b8553659824b91776a58
Shon Qwezon <shonzbso@gmail.com>
2024-12-02 22:08:07 +0300

    fix: add return exception

--- app/db/scripts/procedures/client/update_client_balance.sql
+++ app/db/scripts/procedures/client/update_client_balance.sql
@@ -5,5 +5,8 @@ BEGIN
     UPDATE client SET balance = new_balance WHERE id = client_id;
     RAISE NOTICE 'Balance of client "%" has been updated successfully (new balance = %).',
         client_id, new_balance;
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'Client "%" does not exist', client_id;
+    END IF;
 END;
 $$ LANGUAGE plpgsql;
