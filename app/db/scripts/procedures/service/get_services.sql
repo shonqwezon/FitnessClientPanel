86d843cbcf7f1838943f5aa8f8087ab383fd3fdf
Shon Qwezon <shonzbso@gmail.com>
2024-12-02 16:41:55 +0300

    fix: add cost field

--- app/db/scripts/procedures/service/get_services.sql
+++ app/db/scripts/procedures/service/get_services.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE FUNCTION app.get_services()
-RETURNS TABLE (id INTEGER, description TEXT)
+RETURNS TABLE (id INTEGER, description TEXT, cost NUMERIC(5, 2))
 SECURITY DEFINER
 AS $$
 BEGIN
-    RETURN QUERY SELECT s.id, s.description FROM service AS s;
+    RETURN QUERY SELECT s.id, s.description, s.cost FROM service AS s;
 END;
 $$ LANGUAGE plpgsql;
