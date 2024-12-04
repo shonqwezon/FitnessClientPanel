f43d583a0aa161572c267f22baae85807faea4cf
Shon Qwezon <shonzbso@gmail.com>
2024-12-04 20:11:35 +0300

    fix: add sportcenter_id

--- app/db/scripts/procedures/manager/get_manager.sql
+++ app/db/scripts/procedures/manager/get_manager.sql
@@ -3,6 +3,6 @@ RETURNS TABLE (fullname VARCHAR(50), assign_date TIMESTAMP, email VARCHAR(50))
 SECURITY DEFINER
 AS $$
 BEGIN
-    RETURN QUERY SELECT m.fullname, m.assign_date, m.email FROM manager AS m;
+    RETURN QUERY SELECT m.fullname, m.assign_date, m.email, m.sportcenter_id FROM manager AS m;
 END;
 $$ LANGUAGE plpgsql;
