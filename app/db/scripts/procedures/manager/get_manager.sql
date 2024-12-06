9726c8632fa19f014e4819e3e7278561c15905d0
Shon Qwezon <shonzbso@gmail.com>
2024-12-06 17:54:24 +0300

    feat: return sportcenter name

--- app/db/scripts/procedures/manager/get_manager.sql
+++ app/db/scripts/procedures/manager/get_manager.sql
@@ -1,8 +1,10 @@
 CREATE OR REPLACE FUNCTION app.get_manager()
-RETURNS TABLE (fullname VARCHAR(50), assign_date TIMESTAMP, email VARCHAR(50), sportcenter_id INTEGER)
+RETURNS TABLE (fullname VARCHAR(50), assign_date TIMESTAMP, email VARCHAR(50), sportcenter_id INTEGER, sportcenter_name VARCHAR(50))
 SECURITY DEFINER
 AS $$
 BEGIN
-    RETURN QUERY SELECT m.fullname, m.assign_date, m.email, m.sportcenter_id FROM manager AS m;
+    RETURN QUERY SELECT m.fullname, m.assign_date, m.email, m.sportcenter_id, s.name
+        FROM manager AS m
+        JOIN sportcenter AS s ON s.id = m.sportcenter_id;
 END;
 $$ LANGUAGE plpgsql;
