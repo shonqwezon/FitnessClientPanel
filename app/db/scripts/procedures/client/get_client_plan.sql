982b816547d207f347558b6a2fff7c4f6df913d6
Shon Qwezon <shonzbso@gmail.com>
2024-12-07 21:42:36 +0300

    fix: забыл

--- app/db/scripts/procedures/client/get_client_plan.sql
+++ app/db/scripts/procedures/client/get_client_plan.sql
@@ -1,9 +1,11 @@
 CREATE OR REPLACE FUNCTION app.get_client_plan(in_client_id INTEGER)
-RETURNS TABLE(plan_id INTEGER, plan_begin_date DATE, plan_end_date DATE)
+RETURNS TABLE (plan_id INTEGER, plan_begin_date DATE, plan_end_date DATE)
 SECURITY DEFINER
 AS $$
 BEGIN
-    RETURN QUERY cp.plan_id, cp.plan_begin_date, cp.plan_end_date FROM client_plan AS cp WHERE in_client_id = cp.client_id;
+    RETURN QUERY SELECT cp.plan_id, cp.plan_begin_date, cp.plan_end_date
+        FROM client_plan AS cp
+        WHERE in_client_id = cp.client_id;
 
     IF NOT FOUND THEN
         RAISE EXCEPTION 'Plan of client "%" does not exist', in_client_id;
